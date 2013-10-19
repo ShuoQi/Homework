dc1bf07d13ce2843981e24f629bebf4805777f9b
ShuoQi <qs0727@qq.com>
2013-10-19 10:54:55 +0800

    第四次作业

---
+++ HomeWork/HomeWork/H_4_1.cpp
@@ -0,0 +1,15 @@
+﻿#include<iostream>
+class safearray{
+public:
+	safearray(int a,int b){
+		c[a]=b;
+		if(a>=100){
+			printf("数组越界");
+		}
+	}
+private:
+	int c[100];
+};
+int main(){
+	safearray e(101,4);
+}
---
+++ HomeWork/HomeWork/H_4_2.cpp
@@ -0,0 +1,18 @@
+﻿#include<malloc.h>
+struct node{
+	int x;
+	node * next;
+};
+struct Stack{
+	node * p;
+};
+void push(Stack s,int a){
+	node * b=(node*)malloc(sizeof(node));
+	b->x=a;
+	if(s.p==nullptr){
+		s.p=b;
+	}
+	else{
+		s.p->next=b;
+	}
+}
